99ccd9a78354a5f1ea1855360fafe939d1f25929
Jordan Kasper <jordan@dds.mil>
2018-04-11 15:27:48 -0400

    Added setup for doing GH issue creation on pivotal story creation and basic test

--- src/pivotal.js
+++ src/pivotal.js
@@ -1,41 +1,63 @@
 
 const Router = require('koa-router');
+const request = require('request-promise-native');
 
 const router = new Router({
   prefix: '/pivotal'
 });
 
-router.use((ctx, next) => {
+router.use(async (ctx, next) => {
   if (!ctx.request.body || typeof(ctx.request.body) !== 'object') {
     ctx.status = 400;
     ctx.body = 'Bad webhook data from Pivotal';
-    console.error(`Bad webhook data from Pivotal (first 300 bytes): ${ctx.request.body.substr(0,300)}`);
+    console.error(`Bad webhook data from Pivotal (first 300 bytes): ${JSON.stringify(ctx.request.body).substr(0,300)}`);
     return;
   }
-  next();
+  await next();
 });
 
-router.use((ctx, next) => {
+router.use(async (ctx, next) => {
   if (!handlers[ctx.request.body.kind] || typeof(handlers[ctx.request.body.kind]) !== 'function') {
     ctx.status = 200;
+    ctx.type = 'text';
     ctx.body = 'No handler for request kind';
-    console.log(`No handler for request kind: ${ctx.request.body.kind.substr(0,100)}`);
+    console.log(`No handler for request kind: ${ctx.request.body.kind && ctx.request.body.kind.substr(0,100)}`);
     return;
   }
-  next();
+  await next();
 });
 
-router.post('/', (ctx) => {
-  const msg = handlers[ctx.request.body.kind]();
+router.post('/', async (ctx) => {
 
-  ctx.status = 200;
-  ctx.type = 'json';
-  ctx.body = { msg };
+  try {
+    const msg = await handlers[ctx.request.body.kind](ctx.request.body)
+    console.log('request handled...', msg);
+    ctx.status = 200;
+    ctx.type = 'json';
+    ctx.body = { msg };
+  } catch(err) {
+    console.error(`Error from GitHub API for ${ctx.request.body.kind}`, err);
+    ctx.status = 200;
+    ctx.type = 'json';
+    ctx.body = { msg: 'Error while handling Pivotal webhook', err };
+  }
 });
 
 const handlers = {
-  story_create_activity: (data) => {
-    return 'Added new GH issue'
+  story_create_activity: async (data) => {
+    const creation = data.changes.filter((change) => change.change_type === 'create')[0];
+
+    const response = await request({
+      method: 'POST',
+      uri: 'https://api.github.com/repos/dod-ccpo/atat/issues',
+      body: {
+        title: creation.new_values.name,
+        body: creation.new_values.description
+      },
+      json: true
+    });
+
+    return `Added new GH issue: ID=${response.id} TS=${response.created_at}`;
   }
 };
 
--- test/pivotal.spec.js
+++ test/pivotal.spec.js
@@ -1,5 +1,6 @@
 
 const supertest = require('supertest');
+const nock = require('nock');
 const expect = require('chai').expect;
 const app = require('../src/index.js');
 const creation = require('./pivotal-create.json');
@@ -18,14 +19,49 @@ describe('pivotal webhook', () => {
     server.close(done);
   });
 
+  it('should skip unsupported webhook types', async () => {
+    const response = await request
+      .post('/pivotal')
+      .send({ kind: 'foobar' });
+
+    expect(response.status).to.equal(200);
+    expect(response.type).to.equal('text/plain');
+    expect(response.text).to.equal('No handler for request kind');
+
+    return response;
+  });
+
+  it('should fail for GET requests', async () => {
+    const response = await request
+      .get('/pivotal');
+    expect(response.status).to.equal(404);
+    return response;
+  });
+
   it('should respond with text on story creation', async () => {
+    nock('https://api.github.com')
+      .post('/repos/dod-ccpo/atat/issues')
+      .reply(200, {
+        id: 13,
+        state: 'open',
+        title: 'Found a bug',
+        body: 'I\'m having a problem with this.',
+        user: {
+          id: 27,
+          login: 'octocat'
+        },
+        created_at: '2018-04-10T13:33:48Z',
+        updated_at: '2018-04-10T13:33:48Z'
+      });
+
     const response = await request
       .post('/pivotal')
       .send(creation);
+
     expect(response.status).to.equal(200);
     expect(response.type).to.equal('application/json');
     expect(response.body).to.be.a('object');
-    expect(response.body.msg).to.equal('Added new GH issue');
+    expect(response.body.msg).to.equal('Added new GH issue: ID=13 TS=2018-04-10T13:33:48Z');
 
     return response;
   });
